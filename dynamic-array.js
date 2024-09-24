c9ee60d7bd128b76874024c60f45e1266ebce537
Dancode-188 <danbitengo@gmail.com>
2024-09-24 23:44:32 +0300

    Implement the read, push, pop, shift, unshift, indexOf, and resize methods in the DynamicArray class.

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,74 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
+    this.length = 0;
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here
+    if (this.length === this.capacity) {
+      this.resize();
+    }
+    this.data[this.length] = val;
+    this.length++;
   }
 
 
   pop() {
-
-    // Your code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const last = this.data[this.length - 1];
+    this.data[this.length - 1] = undefined;
+    this.length--;
+    return last;
   }
 
   shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    const first = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
 
-    // Your code here
+    this.length--;
+    return first;
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length >= this.capacity) {
+      this.resize();
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
-
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   resize() {
-
-    // Your code here
+    this.capacity *= 2;
+    const newData = new Array(this.capacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
   }
 
 }
